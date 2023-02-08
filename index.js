c01e5b48cadae88c2d7d23476b78bf73ddc9cb4f
Paweł Stępień <finestedm@gmail.com>
2023-02-08 17:40:24 +0100

    fixed index.js not being found

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const server = http.createServer((req, res) => {
 
     const filePath = path.join(
         __dirname,
-        '..',
         'src',
         req.url === '/' ? 'index.html' : req.url
     )
@@ -16,7 +15,7 @@ const server = http.createServer((req, res) => {
     fs.readFile(filePath, (err, content) => {
         if (err) {
             if (err.code === 'ENOENT') {
-                fs.readFile(path.join(__dirname, '..', 'src', '404.html'), (err, content) => {
+                fs.readFile(path.join(__dirname, 'src', '404.html'), (err, content) => {
                     res.writeHead(200, { 'ContentType': 'text/html' });
                     res.end(content, 'utf-8')
                 })
