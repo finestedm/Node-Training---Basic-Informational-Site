f4a45d40fca479f523c5d72bd410f6a887b859f1
Pawel <finestedm@gmail.com>
2023-02-22 22:53:21 +0100

    un==initial commit with first step of app conversion to express

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
-const http = require('http')
-const path = require('path')
-const fs = require('fs')
+// const http = require('http')
+// const path = require('path')
+// const fs = require('fs')
 
-const server = http.createServer((req, res) => {
+const express = require('express')
+const app = express()
 
-    const filePath = path.join(
-        __dirname,
-        'src',
-        req.url === '/' ? 'index.html' : req.url
-    )
+app.listen(3000)
 
-    let extname = path.extname(filePath)
-    
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                fs.readFile(path.join(__dirname, 'src', '404.html'), (err, content) => {
-                    res.writeHead(200, { 'ContentType': 'text/html' });
-                    res.end(content, 'utf-8')
-                })
-            } else {
-                res.writeHead(500);
-                res.end(conent, 'utf-8');
-            }
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' })
-            res.end(content, 'utf-8');
-        }
-    })
-
+app.get('/', (req, res) => {
+    res.send('Index')
+    // res.render('./src/index')
 })
 
-const PORT = process.env.PORT || 5000;
+// const server = http.createServer((req, res) => {
 
-server.listen(PORT, () => console.log('server is running on port: ', PORT))
+//     const filePath = path.join(
+//         __dirname,
+//         'src',
+//         req.url === '/' ? 'index.html' : req.url
+//     )
+
+//     let extname = path.extname(filePath)
+    
+//     fs.readFile(filePath, (err, content) => {
+//         if (err) {
+//             if (err.code === 'ENOENT') {
+//                 fs.readFile(path.join(__dirname, 'src', '404.html'), (err, content) => {
+//                     res.writeHead(200, { 'ContentType': 'text/html' });
+//                     res.end(content, 'utf-8')
+//                 })
+//             } else {
+//                 res.writeHead(500);
+//                 res.end(conent, 'utf-8');
+//             }
+//         } else {
+//             res.writeHead(200, { 'Content-Type': 'text/html' })
+//             res.end(content, 'utf-8');
+//         }
+//     })
+
+// })
